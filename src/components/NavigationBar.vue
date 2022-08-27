7ed91ddd8472589b7fc0320cd6fb0ca3c6962d66
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-27 16:17:33 +0200

    feat: add navigation bar component

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <p>Best Cheese in Town</p>
+    <ul>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Crafters</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
