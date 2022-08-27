129b6e34c63e89e0472e7b13d8a6c2c8a3ddd1f7
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-27 16:22:30 +0200

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <NavigationBar></NavigationBar>
+
+  <Footer></Footer>
 </template>
 
 <script>
 import NavigationBar from "./components/NavigationBar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     NavigationBar,
+    Footer,
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <address>21st Rochefort Street</address>
+  </footer>
+  <ul>
+    <li><a href="#">Shipping Rates</a></li>
+    <li><a href="#">Work with US</a></li>
+    <li><a href="#">More Cheese</a></li>
+  </ul>
+</template>
+
+<script>
+export default {};
+</script>
