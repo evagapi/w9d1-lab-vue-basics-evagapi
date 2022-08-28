9a64c834958bcc6f9e54ae7dffd4602d518f8f1a
Eva Garcia Pinos <eva.gapi@gmail.com>
2022-08-28 18:12:03 +0200

    fix: make background dark when scrolling to the edge

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <NavigationBar></NavigationBar>
-  <main class="px-16 py-8">
+  <main class="px-16 py-8 bg-white">
     <p class="bg-teal-100 p-4 mb-8">2 + 2 : {{ 2 + 2 }}</p>
     <p class="bg-pink-100 p-4 mb-8">print: {{ print() }}</p>
     <Form></Form>
